d604290a7dc97dcd51bebb5e3ab8cd135c25fc32
Raf. Mavragani <efimavragani99@gmail.com>
2025-07-10 16:02:42 +0300

    hotfix and AppTask UI updaded

--- AnimalCenterAPI/DTO/AppTaskDTO.cs
+++ AnimalCenterAPI/DTO/AppTaskDTO.cs
@@ -9,7 +9,7 @@ namespace AnimalCenterAPI.DTO
         [NotNull]
         public int Id { get; set; }
         [Required (ErrorMessage = " The field is required.") ]
-        public string Name { get; set; } = null!;
+        public string? Name { get; set; } 
 
         public string? Description { get; set; }
 
