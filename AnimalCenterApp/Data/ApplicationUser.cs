0a818296ecca93d59a4e79c78a3f5320fde7a220
Raf. Mavragani <efimavragani99@gmail.com>
2025-07-24 12:38:16 +0300

    hotfix

--- AnimalCenterApp/Data/ApplicationUser.cs
+++ AnimalCenterApp/Data/ApplicationUser.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace AnimalCenterApp.Data
 {
-    // Add profile data for application users by adding properties to the ApplicationUser class
+    
     public class ApplicationUser : IdentityUser
     {
     }
